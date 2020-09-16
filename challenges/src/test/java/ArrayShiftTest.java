8554a51a564038f75a0b87b18d77c05a9c80fbe6
Kamit Satkeev <kamit.satkeev@gmail.com>
2020-09-15 19:22:26 -0700

    test

--- challenges/src/test/java/ArrayShiftTest.java
+++ challenges/src/test/java/ArrayShiftTest.java
@@ -7,7 +7,9 @@ public class ArrayShiftTest {
     @Test public void testArrayShift() {
         ArrayShift classUnderTest = new ArrayShift();
         int[] array = {2,5,7,8,9};
+        //declare random array
         int n = 10;
+        //define random n
         int[] outputArray = classUnderTest.ShiftArray(array, n);
         int middle = outputArray.length/2;
         assertEquals(n, outputArray[middle]);
