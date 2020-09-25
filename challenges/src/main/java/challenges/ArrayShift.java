c8387f4f9d1d7d3a1abe2db1f6d14a24fd987413
Kamit Satkeev <kamit.satkeev@gmail.com>
2020-09-24 21:37:31 -0700

    resubmit

--- challenges/src/main/java/challenges/ArrayShift.java
+++ challenges/src/main/java/challenges/ArrayShift.java
@@ -5,15 +5,15 @@ public class ArrayShift {
     public static int[] ShiftArray ( int[] InArray, int newValue){
 
 
+
         int[] OutArray = new int[InArray.length + 1];
-        for (int i = 0; i < InArray.length + 1; i++) {
-            if (i < InArray.length - 1) {
-                OutArray[i] = InArray[i];
-            } else if (i == newValue) {
-                OutArray[i] = newValue;
-            } else {
-                OutArray[i] = InArray[i - 1];
-            }
+        int middle = InArray.length / 2 + 1;
+        for (int i = 0; i < middle; i++) {
+            OutArray[i] = InArray[i];
+        }
+        OutArray[middle] = newValue;
+        for (int i = middle + 1; i < OutArray.length; i++) {
+            OutArray[i] = InArray[i - 1];
         }
         return OutArray;
 
