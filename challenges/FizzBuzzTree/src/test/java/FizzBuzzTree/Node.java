18add208d7dfd1e00f43a684c1fa34a095174950
Kamit Satkeev <kamit.satkeev@gmail.com>
2020-10-07 21:21:59 -0700

    challenge 18

---
+++ challenges/FizzBuzzTree/src/test/java/FizzBuzzTree/Node.java
@@ -0,0 +1,11 @@
+package FizzBuzzTree;
+
+public class Node extends FizzBuzzTree.Node {
+    public Node(int i) {
+        super();
+    }
+
+    public Node(int i, FizzBuzzTree.Node node, FizzBuzzTree.Node node1) {
+        super();
+    }
+}
