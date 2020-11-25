5d476d7a735bea91c300eac37a962ed23bf2c6c2
Kamit Satkeev <kamit.satkeev@gmail.com>
2020-11-25 10:32:52 -0800

    add whiteboard

--- challenge401/src/main/java/challenges/insertionsort/insertSort.java
+++ challenge401/src/main/java/challenges/insertionsort/insertSort.java
@@ -1,20 +1,20 @@
-package insertionsort;
+package challenges.insertionsort;
 
 
-public class insertSort {                          //this is the java class (we are declaring the class).
-    public void selectionSort(int[] arr){  //we are defining the method which takes in an int array as its params.
-        int n = arr.length;                        //we are declaring n as an integer, assigning the arr.length to be its size
-        for(int i = 0; i < n; i++){                //we are creating a for loop utilizing i, which is checked against the size of n
-            int min = i;                           //declaring a new integer of "min" which is assigned to i
-            for(int j = i + 1; j < n; j++){        //create a for loop, assigning j to i + 1. (j starts one ahead of i) which is then checked against the length of n
-                if(arr[j] < arr[min])              //if statement, if J's index is smaller than min's index, do next.
-                    min = j;                           //then min becomes J
+public class insertSort {
+    public static void insertionSort(int[] arr) {
+        int j;
+        int temp;
+        for (int i = 1; i < arr.length; i++) {
+            j = i - 1;
+            temp = arr[i];
+
+            while (j >= 0 && temp < arr[j]) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
             }
-            int temp = arr[min];                   //Declaring integer temp, assigning it to min's index.
-            arr[min] = arr[i];                     //we are re-assigning min's index to i's index
-            arr[i] = temp;                         //we are re-assigning i's index to temp's value
+
+            arr[j + 1] = temp;
         }
-//        return arr;
     }
-//    System.out.println("print this");            // sout is broken?
 }
